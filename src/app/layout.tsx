c9f17e68b36770854ed0a52d71dc3deb190eb9d6
nandeesh_007 <nandeesh.db@bulkbeings.co>
2025-01-24 13:54:37 +0530

    hot-fix:  added invisible clrek authentication div

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { ClerkProvider } from "@clerk/nextjs";
+import { Toaster } from "@/components/ui/toaster";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -29,7 +30,10 @@ export default function RootLayout({
         <body
           className={`${geistSans.variable} ${geistMono.variable} antialiased`}
         >
+          <div id="clerk-captcha"></div>
+
           {children}
+          <Toaster />
         </body>
       </html>
     </ClerkProvider>
