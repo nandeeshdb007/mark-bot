6de70168aeabf53c016729ccea4ac7c554f89e65
nandeesh_007 <nandeesh.db@bulkbeings.co>
2025-01-24 23:21:58 +0530

    created use side bar hook

---
+++ src/hooks/use-side-bar.ts
@@ -0,0 +1,74 @@
+"use client";
+import { useEffect, useState } from "react";
+import { useChatContext } from "../context/use-chat-context";
+import {
+  onGetConversationMode,
+  onToggleRealtine,
+} from "@/actions/conversation";
+import { usePathname } from "next/navigation";
+import { useRouter } from "next/router";
+import { useToast } from "@/hooks/use-toast";
+import { useClerk } from "@clerk/nextjs";
+
+const useSideBar = () => {
+  const [expand, setExpand] = useState<boolean | undefined>(undefined);
+  const router = useRouter();
+  const pathname = usePathname();
+  const { toast } = useToast();
+  const [realtime, setRealtime] = useState<boolean>(false);
+  const [loading, setLoading] = useState<boolean>(false);
+
+  const { chatRoom } = useChatContext();
+
+  const onActivateRealTime = async (e: React.ChangeEvent<HTMLInputElement>) => {
+    try {
+      if (!chatRoom) return null;
+      const realTime = await onToggleRealtine(
+        chatRoom,
+        e?.target?.ariaChecked == "true" ? false : true
+      );
+      if (realTime) {
+        setRealtime(realTime.chatRoom.live);
+        toast({
+          title: "Success",
+          description: realTime.message,
+        });
+      }
+    } catch (error) {
+      console.log("onActivateRealTime", error);
+    }
+  };
+
+  const onGetCurrentMode = async () => {
+    setLoading(true);
+    const mode = await onGetConversationMode(chatRoom!);
+    if (mode) {
+      setRealtime(mode.live);
+      setLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    if (chatRoom) {
+      onGetCurrentMode();
+    }
+  }, [chatRoom]);
+
+  const page = pathname.split("/").pop();
+  const { signOut } = useClerk();
+  const onSignOut = () => signOut(() => router.push("/"));
+  const onExpand = () => setExpand((prev) => !prev);
+
+  return {
+    expand,
+    onExpand,
+    page,
+    onSignOut,
+    realtime,
+    onActivateRealTime,
+    chatRoom,
+    loading,
+  };
+};
+
+export default useSideBar;
