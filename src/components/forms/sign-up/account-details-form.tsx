652744d58568b91f5e217f41ab741bd9b261208d
nandeesh_007 <nandeesh.db@bulkbeings.co>
2025-01-24 13:31:35 +0530

    integrated dynamic form in action

--- src/components/forms/sign-up/account-details-form.tsx
+++ src/components/forms/sign-up/account-details-form.tsx
@@ -1,5 +1,7 @@
+import { USER_REGISTRATION_FORM } from "@/constant/forms";
 import React, { FC } from "react";
 import { FieldErrors, FieldValues, UseFormRegister } from "react-hook-form";
+import FormGenerator from "../form-generator";
 
 const AccountDetailsForm: FC<{
   register: UseFormRegister<FieldValues>;
@@ -7,10 +9,17 @@ const AccountDetailsForm: FC<{
 }> = ({ register, errors }) => {
   return (
     <>
-      <h2 className="text-gravel md:text-4xl font-bold">
-        Account Details
-      </h2>
+      <h2 className="text-gravel md:text-4xl font-bold">Account Details</h2>
       <p className="text-iridium md:text-sm">Enter your email and password</p>
+      {USER_REGISTRATION_FORM.map((field) => (
+        <FormGenerator
+          key={field.id}
+          {...field}
+          errors={errors}
+          register={register}
+          name={field.name}
+        />
+      ))}
     </>
   );
 };
