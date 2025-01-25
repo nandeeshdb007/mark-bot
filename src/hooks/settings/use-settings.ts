525bc2c7a72a32161245e686493ef129390551f8
nandeesh_007 <nandeesh.db@bulkbeings.co>
2025-01-25 14:26:54 +0530

    created useChangePassword hook functionality

--- src/hooks/settings/use-settings.ts
+++ src/hooks/settings/use-settings.ts
@@ -1,10 +1,54 @@
-
+import {
+  ChangePasswordProps,
+  ChangePasswordSchema,
+} from "@/schemas/auth.schema";
+import { zodResolver } from "@hookform/resolvers/zod";
 import { useTheme } from "next-themes";
+import { useForm } from "react-hook-form";
+import { useToast } from "../use-toast";
+import { useState } from "react";
+import { onUpdatedPassword } from "@/actions/settings";
 
 export const useThemeMode = () => {
-  const { setTheme, theme } = useTheme()
+  const { setTheme, theme } = useTheme();
   return {
     setTheme,
     theme,
-  }
-}
+  };
+};
+
+export const useChangePassword = () => {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+    reset,
+  } = useForm<ChangePasswordProps>({
+    resolver: zodResolver(ChangePasswordSchema),
+    mode: "onChange",
+  });
+
+  const { toast } = useToast();
+  const [loading, setLoading] = useState(false);
+
+  const onChangePassword = handleSubmit(async (values) => {
+    try {
+      setLoading(true);
+      const updated = await onUpdatedPassword(values.password);
+      if (updated?.status == 200) {
+        reset();
+        setLoading(false);
+        toast({ title: "Success", description: updated.message });
+      }
+    } catch (error) {
+      console.log("onChangePassword", onChangePassword);
+    }
+  });
+
+  return {
+    register,
+    errors,
+    loading,
+    onChangePassword,
+  };
+};
--- src/schemas/auth.schema.ts
+++ src/schemas/auth.schema.ts
@@ -15,6 +15,11 @@ export type UserLoginProps = {
   password: string;
 };
 
+export type ChangePasswordProps = {
+  password: string;
+  confirmPassword: string;
+};
+
 export const UserRegistrationSchema: ZodType<UserRegistrationProps> = z
   .object({
     type: z.string().min(1),
@@ -49,3 +54,22 @@ export const UserLoginSchema: ZodType<UserLoginProps> = z.object({
   email: z.string().email({ message: "Incorrect email format" }),
   password: z.string(),
 });
+
+export const ChangePasswordSchema: ZodType<ChangePasswordProps> = z
+  .object({
+    password: z
+      .string()
+      .min(8, { message: "Your password must be atleast 8 characters long" })
+      .max(64, {
+        message: "Your password can not be longer then 64 characters long",
+      })
+      .refine(
+        (value) => /^[a-zA-Z0-9_.-]*$/.test(value ?? ""),
+        "password should contain only alphabets and numbers"
+      ),
+    confirmPassword: z.string(),
+  })
+  .refine((schema) => schema.password === schema.confirmPassword, {
+    message: "passwords do not match",
+    path: ["confirmPassword"],
+  });
