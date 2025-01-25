4b903d3e8d4f8c9e45d84f7b20f22187fd77664e
nandeesh_007 <nandeesh.db@bulkbeings.co>
2025-01-25 13:49:04 +0530

    Created useThemeMode hook

--- src/hooks/settings/use-settings.ts
+++ src/hooks/settings/use-settings.ts
@@ -1,7 +1,10 @@
-import { UploadClient } from "@uploadcare/upload-client";
-const upload = new UploadClient({
-  publicKey: process.env.NEXT_PUBLIC_UPLOAD_CARE_PUBLIC_KEY as string,
-});
+import { useTheme } from "next-themes";
+
 export const useThemeMode = () => {
-  
+  const { setTheme, theme } = useTheme();
+  return{
+    setTheme,
+    theme
+  }
+
 };
