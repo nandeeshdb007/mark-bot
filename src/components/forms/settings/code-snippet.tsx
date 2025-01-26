b755f71761f452ad40a59f8ed4f57876dbd929c3
nandeesh_007 <nandeesh.db@bulkbeings.co>
2025-01-26 17:02:09 +0530

    hot-fix

--- src/components/forms/settings/code-snippet.tsx
+++ src/components/forms/settings/code-snippet.tsx
@@ -1,15 +1,15 @@
-'use client'
-import Section from '@/components/section-label'
-import { useToast } from '@/hooks/use-toast'
-import { Copy } from 'lucide-react'
-import React from 'react'
+"use client";
+import Section from "@/components/section-label";
+import { useToast } from "@/hooks/use-toast";
+import { Copy } from "lucide-react";
+import React from "react";
 
 type Props = {
-  id: string
-}
+  id: string;
+};
 
 const CodeSnippet = ({ id }: Props) => {
-  const { toast } = useToast()
+  const { toast } = useToast();
   const snippet = `
     const iframe = document.createElement("iframe");
     
@@ -28,18 +28,18 @@ const CodeSnippet = ({ id }: Props) => {
         }
     ')
     
-    iframe.src = "http://localhost:3000/chatbot"
+    iframe.src = "${process.env.PRODUCTION_URL}/chatbot"
     iframe.classList.add('chat-frame')
     document.body.appendChild(iframe)
     
     window.addEventListener("message", (e) => {
-        if(e.origin !== "http://localhost:3000") return null
+        if(e.origin !== "${process.env.PRODUCTION_URL}") return null
         let dimensions = JSON.parse(e.data)
         iframe.width = dimensions.width
         iframe.height = dimensions.height
-        iframe.contentWindow.postMessage("${id}", "http://localhost:3000/")
+        iframe.contentWindow.postMessage("${id}", "${process.env.PRODUCTION_URL}/")
     })
-        `
+        `;
 
   return (
     <div className="mt-10 flex flex-col gap-5 items-start">
@@ -51,11 +51,11 @@ const CodeSnippet = ({ id }: Props) => {
         <Copy
           className="absolute top-5 right-5 text-gray-400 cursor-pointer"
           onClick={() => {
-            navigator.clipboard.writeText(snippet)
+            navigator.clipboard.writeText(snippet);
             toast({
-              title: 'Copied to clipboard',
-              description: 'You can now paste the code inside your website',
-            })
+              title: "Copied to clipboard",
+              description: "You can now paste the code inside your website",
+            });
           }}
         />
         <pre>
@@ -63,7 +63,7 @@ const CodeSnippet = ({ id }: Props) => {
         </pre>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default CodeSnippet
+export default CodeSnippet;
