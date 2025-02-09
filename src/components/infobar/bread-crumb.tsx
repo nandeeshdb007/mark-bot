2412cdc9254282a821cb86b3fbb1edb0d580f93c
nandeesh_007 <nandeesh.db@bulkbeings.co>
2025-02-09 20:15:51 +0530

    fixed bread crumb name display bug

--- src/components/infobar/bread-crumb.tsx
+++ src/components/infobar/bread-crumb.tsx
@@ -1,10 +1,12 @@
 "use client";
-import useSideBar from "@/hooks/side-bar/use-side-bar";
 import React from "react";
 import { Loader } from "../loader";
 import { Switch } from "../ui/switch";
+import useSideBar from "@/hooks/side-bar/use-side-bar";
 
-const BreadCrumb = () => {
+type Props = {};
+
+const BreadCrumb = (props: Props) => {
   const {
     chatRoom,
     expand,
@@ -15,40 +17,33 @@ const BreadCrumb = () => {
     onSignOut,
     realtime,
   } = useSideBar();
-
-  let content: string;
-  switch (page) {
-    case "settings":
-      content = "Mange your account settings, prefernce and integrations";
-    case "dashboard":
-      content =
-        "A detailed overview of your metrics, usage, customers and more";
-    case "appointment":
-      content = " View and edit all your appointments";
-    case "email-marketing":
-      content = " Send bulk emails to your customers";
-    case "integration":
-      content = "Connect third party application into Mark AI";
-    default:
-      content =
-        "Modify domain settings, change chatbot options, enter sales questions and train your bot o what you want it to...";
-  }
   return (
-    <div className="flex flex-col">
+    <div className="flex flex-col ">
       <div className="flex gap-5 items-center">
-        <h2 className="text-3xl font-bold capitalize">
-          {page === "conversation" && chatRoom && (
-            <Loader loading={loading} className="p-0 inline">
-              <Switch
-                defaultChecked={realtime}
-                onClick={(e: any) => onActivateRealTime(e)}
-                className="data-[state=checked]:bg-orange data-[state=unchecked]:bg-peach"
-              />
-            </Loader>
-          )}
-        </h2>
+        <h2 className="text-3xl font-bold capitalize">{page}</h2>
+        {page === "conversation" && chatRoom && (
+          <Loader loading={loading} className="p-0 inline">
+            <Switch
+              defaultChecked={realtime}
+              onClick={(e) => onActivateRealTime(e)}
+              className="data-[state=checked]:bg-orange data-[state=unchecked]:bg-peach"
+            />
+          </Loader>
+        )}
       </div>
-      <p className="text-gray-500 text-sm">{content}</p>
+      <p className="text-gray-500 text-sm">
+        {page == "settings"
+          ? "Manage your account settings, preferences and integrations"
+          : page == "dashboard"
+          ? "A detailed overview of your metrics, usage, customers and more"
+          : page == "appointment"
+          ? "View and edit all your appointments"
+          : page == "email-marketing"
+          ? "Send bulk emails to your customers"
+          : page == "integration"
+          ? "Connect third-party applications into Corinna-AI"
+          : "Modify domain settings, change chatbot options, enter sales questions and train your bot to do what you want it to."}
+      </p>
     </div>
   );
 };
