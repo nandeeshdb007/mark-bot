6989d71ce1af449b045e86607f9b1ffe69d279c8
nandeesh_007 <nandeesh.db@bulkbeings.co>
2025-02-15 17:09:34 +0530

    hot-fix

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,19 +1,19 @@
 "use client";
 import { useUser } from "@clerk/nextjs";
 import { useRouter } from "next/navigation";
-import { useEffect } from "react";
+import { useCallback } from "react";
 
 export default function Home() {
   const { isSignedIn } = useUser();
   const router = useRouter();
 
-  useEffect(() => {
+  useCallback(() => {
     if (isSignedIn === true) {
       router.push("/dashboard");
     } else if (isSignedIn === false) {
       router.push("/auth/sign-in");
     }
-  }, [isSignedIn, router]);
+  }, []);
 
-  return null;
+  return <></>;
 }
